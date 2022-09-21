65a2be8e45bbc7376d31727e79a05a0d0dde6333
Schapkov <106531210+Schapkov@users.noreply.github.com>
2022-09-21 13:11:37 +0300

    final

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Duck5 {
 
+    private  Egg6 egg;
+    public Duck5(Egg6 egg6) {
+        this.egg = egg6;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
